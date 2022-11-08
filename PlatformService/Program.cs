afb1fb68f5490ae4e5c99cded8eac975d6dd9b33
vivekpratapsingh <vivekpratap@outlook.in>
2022-11-08 07:06:29 +0530

    mssql configuration

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -13,11 +13,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDBContext>(opt => 
-    opt.UseInMemoryDatabase("InMem")
-);
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<AppDBContext>(opt =>
+    {
+        if (builder.Environment.IsProduction())
+        {
+            Console.WriteLine("--> Using SqlServer Db");
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn"));
+        }
+        else
+        {
+            Console.WriteLine("--> Using InMem Db");
+            opt.UseInMemoryDatabase("InMem");
+        }
+    }
+);
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -38,7 +49,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 Console.WriteLine($"--> CommandService EndPoint {app.Configuration["CommandService"]}");
 
